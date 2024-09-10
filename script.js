8fa453a2fac1ba715c11c32d9c42343767c89614
AbdulRaqeebGit <abdulraqeebkumhar@gmail.com>
2024-09-11 00:35:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var password = "Pass123";
+
+var enterPassword = prompt("Enter Your Password");
+
+if (password===enterPassword) {
+    alert("Correct!")
+}
+else{
+    alert(
+        "In Correct Password"
+    )
+}
